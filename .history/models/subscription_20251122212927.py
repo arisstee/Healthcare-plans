fb33d1f6f9dc48cac356e32b097a5a6f39007bf5
arisstee <152800706+arisstee@users.noreply.github.com>
2025-11-22 21:32:32 +0100

    update

---
+++ .history/models/subscription_20251122212927.py
@@ -0,0 +1,12 @@
+from . import db
+
+class UserSubscription(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    plan_id = db.Column(db.Integer, db.ForeignKey('subscription_plan.id'), nullable=False)
+    start_date = db.Column(db.DateTime, nullable=False)
+    end_date = db.Column(db.DateTime, nullable=False)
+    active = db.Column(db.Boolean, default=True)
+    
+    user = db.relationship('User', back_populates='subscriptions')
+    plan = db.relationship('SubscriptionPlan')
